b89b14b8f1ca35ca9e986c0a902fba7a7fda156b
一二三四五 <jiaomingyu5778@gmail.com>
2022-01-09 21:34:43 +0800

    1. 第一次提交，空项目

---
+++ src/test/java/com/moyu/myadmin/MyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyu.myadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
