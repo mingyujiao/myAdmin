a5926c16fd11450d1a764879e3510ad92f122ad5
jiaomingyu <jiaomingyu5778@gmail.com>
2022-01-18 18:01:10 +0800

    1. sa-token 改为 bean 配置

---
+++ src/main/java/com/moyu/myadmin/config/SaTokenConfigure.java
@@ -0,0 +1,33 @@
+package com.moyu.myadmin.config;
+
+import cn.dev33.satoken.config.SaTokenConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Sa-Token代码方式进行配置
+ * @author jiaomingyu5778@gmail.com
+ * @date 2022/1/18 17:58
+ */
+
+@Configuration
+public class SaTokenConfigure {
+
+    // 获取配置Bean (以代码的方式配置Sa-Token, 此配置会覆盖yml中的配置)
+    @Bean
+    @Primary
+    public SaTokenConfig getSaTokenConfigPrimary() {
+        SaTokenConfig config = new SaTokenConfig();
+        config.setTokenName("moyu-token");             // token名称 (同时也是cookie名称)
+        config.setTimeout(86400L);       // token有效期，单位s 默认30天
+        config.setActivityTimeout(-1);              // token临时有效期 (指定时间内无操作就视为token过期) 单位: 秒
+        config.setIsConcurrent(true);               // 是否允许同一账号并发登录 (为true时允许一起登录, 为false时新登录挤掉旧登录)
+        config.setIsShare(true);                    // 在多人登录同一账号时，是否共用一个token (为true时所有登录共用一个token, 为false时每次登录新建一个token)
+        config.setTokenStyle("uuid");               // token风格
+        config.setIsLog(true);                     // 是否输出操作日志
+        return config;
+    }
+
+}
+
